00b10da368e4008892b91c1e89632e3223c29701
Onigumo <onigumo69@outlook.com>
2021-08-22 23:48:36 +0800

    Add skinned cube

--- DirectX69/src/App.cpp
+++ DirectX69/src/App.cpp
@@ -3,6 +3,7 @@
 #include "Drawable/Melon.h"
 #include "Drawable/Pyramid.h"
 #include "Drawable/Sheet.h"
+#include "Drawable/SkinnedBox.h"
 #include "Surface.h"
 #include "GDIPlusManager.h"
 #include "CleanMath.h"
@@ -47,6 +48,11 @@ App::App()
 					gfx, rng, adist, ddist,
 					odist, rdist
 					);
+			case 4:
+				return std::make_unique<SkinnedBox>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+					);
 			default:
 				assert(false && "bad drawable type in factory");
 				return {};
@@ -62,12 +68,11 @@ App::App()
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,3 }	;
+		std::uniform_int_distribution<int> typedist{ 0,4 }	;
 	};
 
-	Factory f(wnd.GetGraphics());
 	drawables.reserve(nDrawables);
-	std::generate_n(std::back_inserter(drawables), nDrawables, f);
+	std::generate_n(std::back_inserter(drawables), nDrawables, Factory{ wnd.GetGraphics() });
 
 	wnd.GetGraphics().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
@@ -97,7 +102,7 @@ void App::DoFrame()
 	wnd.GetGraphics().ClearBuffer(0.07f, 0.0f, 0.12f);
 	for (auto& d : drawables)
 	{
-		d->Update(dt);
+		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
 		d->Draw(wnd.GetGraphics());
 	}
 	wnd.GetGraphics().EndFrame();
