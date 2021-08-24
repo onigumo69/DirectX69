1183304455a72d19f63b63f16eddf7d60123fde9
Onigumo <onigumo69@outlook.com>
2021-08-24 14:37:58 +0800

    Add dynamic vertex template meta

--- DirectX69/include/Bindable/VertexBuffer.h
+++ DirectX69/include/Bindable/VertexBuffer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Bindable/Bindable.h"
 #include "Macros/GraphicsThrowMacros.h"
+#include "Vertex.h"
 
 class VertexBuffer : public Bindable
 {
@@ -8,7 +9,7 @@ public:
 	template<class V>
 	VertexBuffer(Graphics& gfx, const std::vector<V>& vertices)
 		:
-		stride{ sizeof(V) }
+		stride(sizeof(V))
 	{
 		INFO_MANAGER(gfx);
 
@@ -19,14 +20,28 @@ public:
 		bd.MiscFlags = 0u;
 		bd.ByteWidth = UINT(sizeof(V) * vertices.size());
 		bd.StructureByteStride = sizeof(V);
-
 		D3D11_SUBRESOURCE_DATA sd = {};
 		sd.pSysMem = vertices.data();
 		GFX_THROW_INFO(GetDevice(gfx)->CreateBuffer(&bd, &sd, &pVertexBuffer));
 	}
-	
-	void Bind(Graphics& gfx) noexcept override;
+	VertexBuffer(Graphics& gfx, const meta::VertexBuffer& vbuf)
+		:
+		stride((UINT)vbuf.GetLayout().Size())
+	{
+		INFO_MANAGER(gfx);
 
+		D3D11_BUFFER_DESC bd = {};
+		bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		bd.Usage = D3D11_USAGE_DEFAULT;
+		bd.CPUAccessFlags = 0u;
+		bd.MiscFlags = 0u;
+		bd.ByteWidth = UINT(vbuf.SizeBytes());
+		bd.StructureByteStride = stride;
+		D3D11_SUBRESOURCE_DATA sd = {};
+		sd.pSysMem = vbuf.GetData();
+		GFX_THROW_INFO(GetDevice(gfx)->CreateBuffer(&bd, &sd, &pVertexBuffer));
+	}
+	void Bind(Graphics& gfx) noexcept override;
 protected:
 	UINT stride;
 	Microsoft::WRL::ComPtr<ID3D11Buffer> pVertexBuffer;
--- DirectX69/src/Drawable/AssTest.cpp
+++ DirectX69/src/Drawable/AssTest.cpp
@@ -4,6 +4,7 @@
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
+#include "Vertex.h"
 
 AssTest::AssTest(Graphics& gfx, std::mt19937& rng,
 	std::uniform_real_distribution<float>& adist,
@@ -19,11 +20,12 @@ AssTest::AssTest(Graphics& gfx, std::mt19937& rng,
 
 	if (!IsStaticInitialized())
 	{
-		struct Vertex
-		{
-			dx::XMFLOAT3 pos;
-			dx::XMFLOAT3 n;
-		};
+		using meta::VertexLayout;
+		meta::VertexBuffer vbuf(std::move(
+			VertexLayout{}
+			.Append(VertexLayout::Position3D)
+			.Append(VertexLayout::Normal)
+		));
 
 		Assimp::Importer imp;
 		const auto pModel = imp.ReadFile("models\\suzanne.obj",
@@ -32,14 +34,12 @@ AssTest::AssTest(Graphics& gfx, std::mt19937& rng,
 		);
 		const auto pMesh = pModel->mMeshes[0];
 
-		std::vector<Vertex> vertices;
-		vertices.reserve(pMesh->mNumVertices);
 		for (unsigned int i = 0; i < pMesh->mNumVertices; i++)
 		{
-			vertices.push_back({
-				{ pMesh->mVertices[i].x * scale,pMesh->mVertices[i].y * scale,pMesh->mVertices[i].z * scale },
+			vbuf.EmplaceBack(
+				dx::XMFLOAT3{ pMesh->mVertices[i].x * scale,pMesh->mVertices[i].y * scale,pMesh->mVertices[i].z * scale },
 				*reinterpret_cast<dx::XMFLOAT3*>(&pMesh->mNormals[i])
-				});
+			);
 		}
 
 		std::vector<unsigned short> indices;
@@ -53,7 +53,7 @@ AssTest::AssTest(Graphics& gfx, std::mt19937& rng,
 			indices.push_back(face.mIndices[2]);
 		}
 
-		AddStaticBind(std::make_unique<VertexBuffer>(gfx, vertices));
+		AddStaticBind(std::make_unique<VertexBuffer>(gfx, vbuf));
 
 		AddStaticIndexBuffer(std::make_unique<IndexBuffer>(gfx, indices));
 
@@ -63,12 +63,7 @@ AssTest::AssTest(Graphics& gfx, std::mt19937& rng,
 
 		AddStaticBind(std::make_unique<PixelShader>(gfx, L"PhongPS.cso"));
 
-		const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
-		{
-			{ "Position",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
-			{ "Normal",0,DXGI_FORMAT_R32G32B32_FLOAT,0,12,D3D11_INPUT_PER_VERTEX_DATA,0 },
-		};
-		AddStaticBind(std::make_unique<InputLayout>(gfx, ied, pvsbc));
+		AddStaticBind(std::make_unique<InputLayout>(gfx, vbuf.GetLayout().GetD3DLayout(), pvsbc));
 
 		AddStaticBind(std::make_unique<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
 
